807435a04ae891a3bf4361a076d04e00a79b33ac
Zake <zakeplaybr@gmail.com>
2024-12-09 10:17:08 -0300

    Added graphics.c

---
+++ src/graphics.c
@@ -0,0 +1,28 @@
+#include "global.h"
+
+//Summary Screen
+const u16 gFireRedMenuElements2_Pal[] = INCBIN_U16("graphics/summary_screen/dex_caught_pokeball_and_pokemon_types.gbapal");
+const u8 gFireRedMenuElements_Gfx[] = INCBIN_U8("graphics/summary_screen/dex_caught_pokeball_and_pokemon_types.4bpp");
+const u32 gIconStatusSummaryScreen[] = INCBIN_U32("graphics/summary_screen/icon_status.4bpp.lz");
+const u32 gIconStatusSummaryScreenSpa[] = INCBIN_U32("graphics/summary_screen/icon_status_spa.4bpp.lz");
+const u16 gIconStatusPalSummaryScreen[] = INCBIN_U16("graphics/summary_screen/icon_status.gbapal");
+const u32 gExpBarSummaryScreen[] = INCBIN_U32("graphics/summary_screen/exp_bar.4bpp.lz");
+const u32 gHpBarSummaryScreen[] = INCBIN_U32("graphics/summary_screen/hp_bar.4bpp.lz");
+const u32 gHpBarSummaryScreenSpa[] = INCBIN_U32("graphics/summary_screen/hp_bar_spa.4bpp.lz");
+const u16 gPal0SummaryScreen[] = INCBIN_U16("graphics/summary_screen/hp_bar_0.gbapal");
+const u16 gPal1SummaryScreen[] = INCBIN_U16("graphics/summary_screen/hp_bar_1.gbapal");
+const u16 gPal2SummaryScreen[] = INCBIN_U16("graphics/summary_screen/hp_bar_2.gbapal");
+const u16 gSelectCursorPalette[] = INCBIN_U16("graphics/summary_screen/select_pointer_gfx_left.gbapal");
+const u32 gSelectCursorGfxLeft[] = INCBIN_U32("graphics/summary_screen/select_pointer_gfx_left.4bpp.lz");
+const u32 gSelectCursorGfxRight[] = INCBIN_U32("graphics/summary_screen/select_pointer_gfx_right.4bpp.lz");
+const u16 gShinyIndicatorPalette[] = INCBIN_U16( "graphics/summary_screen/shiny_indicator_gfx.gbapal");
+const u32 gShinyIndicatorGfx[] = INCBIN_U32( "graphics/summary_screen/shiny_indicator_gfx.4bpp.lz");
+const u16 gPalSummaryScreen[] = INCBIN_U16("graphics/summary_screen/palette_bg.gbapal");
+const u32 gTilesSummaryScreen[] = INCBIN_U32("graphics/summary_screen/tiles.4bpp.lz");
+const u32 gMapSummaryScreenBg[] = INCBIN_U32("graphics/summary_screen/bg.bin.lz");
+const u32 gMapSummaryScreenEgg[] = INCBIN_U32("graphics/summary_screen/bg_egg.bin.lz");
+const u32 gMapSummaryScreenKnownMoves[] = INCBIN_U32("graphics/summary_screen/bg_known_moves.bin.lz");
+const u32 gMapSummaryScreenMovesInfo[] = INCBIN_U32("graphics/summary_screen/bg_moves_info.bin.lz");
+const u32 gMapSummaryScreenMoves[] = INCBIN_U32("graphics/summary_screen/bg_moves.bin.lz");
+const u32 gMapSummaryScreenPokemonInfo[] = INCBIN_U32("graphics/summary_screen/bg_pokemon_info.bin.lz");
+const u32 gMapSummaryScreenPokemonSkills[] = INCBIN_U32("graphics/summary_screen/bg_pokemon_skills.bin.lz");
